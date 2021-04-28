5181fed9ce7aea62fba9886ebc58498bd6f995d3
Danny Phan <phanhaidang29102000@gmail.com>
2021-04-29 02:30:14 +0700

    backbone

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
+import React, { useReducer, useEffect } from 'react';
 import './App.css';
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch
+} from "react-router-dom";
+import Register from './components/authentication/Register';
+import Login from './components/authentication/Login';
+import Home from './components/home/Home';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+			<Router>
+				<Switch>
+				<Route path="/" exact component={Home} />
+				<Route path="/login" exact component={Login} />
+				<Route path="/register" exact component={Register} />
+				</Switch>
+			</Router>
+	);
 }
 
 export default App;
